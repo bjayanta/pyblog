b7108909dae95ebdc13a5bbbe4060f831f72ff40
Jayanta Biswas <bjayanta.me@gmail.com>
2020-03-27 11:25:38 +0600

    Add paginatation and get posts by author.

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     # post routes 
     path('', views.PostListView.as_view(), name='blog.home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='blog.user.posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='blog.detail'),
     path('post/create/', views.PostCreateView.as_view(), name='blog.create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='blog.update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='blog.delete'),
 
     # about routes
     path('about/', views.About.as_view(), name='blog.about'),
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 
 from .models import Posts
 
@@ -11,6 +13,7 @@ class PostListView(ListView):
     template_name = 'home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-created_at']
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,6 +22,26 @@ class PostListView(ListView):
         context['meta'] = ''
 
         return context
+
+# Post Views according to author
+class UserPostListView(ListView):
+    model = Posts
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['title'] = 'home'
+        context['meta'] = ''
+
+        return context
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Posts.objects.filter(author=user).order_by('-created_at')
+    
 
 class PostDetailView(DetailView):
     model = Posts
@@ -48,6 +71,53 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
 
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Posts
+    fields = ['title', 'content']
+    login_url = '/account/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['title'] = 'create'
+        context['meta'] = ''
+
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+
+        if self.request.user == post.author:
+            return True
+        
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Posts
+    login_url = '/account/'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['title'] = 'detail'
+        context['meta'] = ''
+
+        return context
+
+    def test_func(self):
+        post = self.get_object()
+
+        if self.request.user == post.author:
+            return True
+        
+        return False
 
 # About View
 class About(View):
